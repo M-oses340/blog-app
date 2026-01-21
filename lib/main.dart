96bf006b6f7f70d84b1687e3499a52d27b5409ba
Moses Omwa <mosesomwa7@gmail.com>
2026-01-21 17:17:06 +0300

    authentication

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
 
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const SignupPage(),
+      home: const SignUpPage(),
     );
   }
 }
