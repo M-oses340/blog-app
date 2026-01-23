92ea04c83d976da8c7f520ec53ebfae614708557
Moses Omwa <mosesomwa7@gmail.com>
2026-01-23 09:03:25 +0300

    supabase authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:blog_app/core/common/cubits/app_user/app_user_cubit.dart';
+import 'package:blog_app/core/common/cubits/app_user_cubit.dart';
 import 'package:blog_app/core/theme/theme.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
